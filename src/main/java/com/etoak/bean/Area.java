d54dcb7e4be2c825b378a3709c99ea766228f44b
luzetong <3057139536@qq.com>
2020-04-20 17:27:41 +0800

    测试完之后提交

---
+++ src/main/java/com/etoak/bean/Area.java
@@ -0,0 +1,14 @@
+package com.etoak.bean;
+
+import lombok.Data;
+
+/**
+ * Created by Administrator on 2020/4/20.
+ */
+@Data
+public class Area {
+    private Integer id;
+    private Integer pid;
+    private Integer sort;
+    private String name;
+}
