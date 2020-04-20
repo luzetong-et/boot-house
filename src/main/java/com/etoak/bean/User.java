e9602d457034cc5188d80d60e35a23f2a4b7a83a
luzetong <3057139536@qq.com>
2020-04-20 14:53:55 +0800

    dev分支开发user.java

---
+++ src/main/java/com/etoak/bean/User.java
@@ -0,0 +1,21 @@
+package com.etoak.bean;
+
+import lombok.Data;
+
+/**
+ * Created by Administrator on 2020/4/20.
+ */
+@Data
+public class User {
+    private Integer id;
+
+    private String name;
+
+    private String password;
+
+    private String  gender;
+
+    private Integer age;
+
+    private String birthday;
+}
