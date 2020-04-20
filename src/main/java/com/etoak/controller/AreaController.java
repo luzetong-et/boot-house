0963587f6883d1be6de491cc274bebf74b9e68b8
luzetong <3057139536@qq.com>
2020-04-20 17:52:16 +0800

    修改areacontroller.java,pom.xml,删除areaimplicitParam注解

--- src/main/java/com/etoak/controller/AreaController.java
+++ src/main/java/com/etoak/controller/AreaController.java
@@ -2,6 +2,10 @@ package com.etoak.controller;
 
 import com.etoak.bean.Area;
 import com.etoak.service.AreaService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +25,9 @@ public class AreaController {
     @Autowired
     AreaService areaService;
     @GetMapping("/queryByPid")
+    @ApiOperation(value="根据父id查询地区列表",notes="根据父id查询地区列表")
+    @ApiImplicitParam(value="父id",name="pid",required = false,defaultValue = "0",
+    paramType = "query")
     public List<Area> queryByPid(@RequestParam(required = false,defaultValue = "0") int pid){
         log.info("pid -{}",pid);
         return areaService.queryByPid(pid);
